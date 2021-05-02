5b204c52013214dbeb4801419e57948dbfacbb30
Travis Geiselbrecht <travisg@gmail.com>
2021-05-02 13:37:08 -0700

    [int] add a simple vector table and one vector for illegal instruction
    Does nothing, but actually catches it and stops the cpu.
    Also move the stack into bss and add a bss zeroer.

--- main.c
+++ main.c
@@ -2,8 +2,16 @@
 #include "uart.h"
 
 // simple test code for qemu 6.0's virt machine for 68k
+extern void m68k_vector_base(void);
+
+static void cpu_early_init(void) {
+    // initialize critical cpu stuff
+    asm("movec %0,%/vbr" :: "r"(&m68k_vector_base));
+}
 
 void _main() {
+    cpu_early_init();
+
     // initialize the uart before the int controller is brought up
     // so we can output something.
     uart_early_init();
@@ -16,6 +24,8 @@ void _main() {
     uart_init();
 
     uart_puts("fin.\n");
+
+    asm("illegal");
 
     for (;;);
 }
